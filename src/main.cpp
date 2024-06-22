bffbc9fddc967215eaed8839f4faa0407328d57f
Kennan Gumbs <kfg021@gmail.com>
2024-06-21 20:00:55 -0400

    add file dialog

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include "gui/mainwindow.hpp"
 
 #include <QApplication>
+#include <QFileDialog>
 
 // Testing can be done using nestest.nes rom:
 // path/to/NES/executable nestest.nes
 int main(int argc, char* argv[]){
     QApplication app(argc, argv);
 
-    std::string filePath = argc > 1 ? argv[1] : "";  
+    // Choose the .nes file to run.
+    // Use input from the command line argument if provided, otherwise choose from file dialog
+    std::string filePath;
+    if(argc > 1){
+        filePath = argv[1];
+    }
+    else{
+        filePath = QFileDialog::getOpenFileName(nullptr, "Choose a .nes file to open.", "", "*.nes").toStdString();
+    }
+    
     MainWindow window(nullptr, filePath);
     window.show();
 
