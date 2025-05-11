d4b030b26573fa8a7aedd8d18b82a8c69385cdc6
Kennan Gumbs <kfg021@gmail.com>
2025-05-11 02:47:44 -0400

    begin apu

---
+++ include/core/apu.hpp
@@ -0,0 +1,24 @@
+#ifndef APU_HPP
+#define APU_HPP
+
+#include <cstdint>
+
+class APU {
+public:
+    APU();
+
+    void write(uint16_t addr, uint8_t value);
+
+    // Handles reads to/writes from 0x4015
+    uint8_t viewStatus();
+    uint8_t readStatus();
+    void writeStatus(uint8_t value);
+
+    // Handles writes to 0x4017
+    void writeFrameCounter(uint8_t value);
+
+    double getAudioSample();
+private:
+};
+
+#endif // APU_HPP
---
+++ src/core/apu.cpp
@@ -0,0 +1,33 @@
+#include "core/apu.hpp"
+
+#include <ctime>
+#include <cmath>
+
+APU::APU() {
+
+}
+
+void APU::write(uint16_t addr, uint8_t value) {
+
+}
+
+
+uint8_t APU::viewStatus() {
+    return 0;
+}
+
+uint8_t APU::readStatus() {
+    return 0;
+}
+
+void APU::writeStatus(uint8_t value) {
+
+}
+
+void APU::writeFrameCounter(uint8_t value) {
+
+}
+
+double APU::getAudioSample() {
+    
+}
