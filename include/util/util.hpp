0617dfc2db7cd4b3cf0777ab86845524b3f628e8
Kennan Gumbs <kfg021@gmail.com>
2025-06-11 14:42:36 -0400

    create bit field template

--- include/util/util.hpp
+++ include/util/util.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <string>
+#include <type_traits>
 
 static constexpr uint32_t KB = (1 << 10);
 
@@ -12,6 +13,47 @@ constexpr uint32_t MASK() {
     static_assert(isPowerOf2, "Input to MASK must be power of 2");
     return x - 1;
 }
+
+template <uint8_t Offset, uint8_t Size, typename T = uint8_t>
+class BitField {
+    static_assert(std::is_unsigned<T>::value, "BitField type must be unsigned");
+    static_assert(Size > 0, "BitField size must be positive");
+    static_assert(Offset + Size <= 8 * sizeof(T), "Bit subset must be fit within BitField type");
+
+public:
+    BitField(T& data) : data(data) {}
+
+    operator T() const { return get(); }
+
+    BitField& operator=(T rhs) { set(rhs); return *this; }
+    BitField& operator=(const BitField& rhs) { set(static_cast<T>(rhs)); return *this; }
+
+    BitField& operator+=(T rhs) { set(get() + rhs); return *this; }
+    BitField& operator-=(T rhs) { set(get() - rhs); return *this; }
+    BitField& operator&=(T rhs) { set(get() & rhs); return *this; }
+    BitField& operator|=(T rhs) { set(get() | rhs); return *this; }
+    BitField& operator^=(T rhs) { set(get() ^ rhs); return *this; }
+
+    BitField& operator<<=(uint8_t rhs) { set(get() << rhs); return *this; }
+    BitField& operator>>=(uint8_t rhs) { set(get() >> rhs); return *this; }
+
+    BitField& operator++() { set(get() + 1); return *this; }
+    BitField& operator--() { set(get() - 1); return *this; }
+
+private:
+    static constexpr T unshiftedMask = (static_cast<T>(1) << Size) - 1;
+    static constexpr T shiftedMask = unshiftedMask << Offset;
+
+    T& data;
+
+    T get() const {
+        return (data >> Offset) & unshiftedMask;
+    }
+
+    void set(T value) {
+        data = (data & ~shiftedMask) | ((value & unshiftedMask) << Offset);
+    }
+};
 
 struct MemoryRange {
     const uint16_t lo, hi;
